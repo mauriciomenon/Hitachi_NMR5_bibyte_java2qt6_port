bc45c8319e03b6fb792677b8edb817fd703869a5
mauriciomenon <54405514+mauriciomenon@users.noreply.github.com>
2023-10-14 14:09:00 -0300

    .

--- import sys.py
+++ import sys.py
@@ -1,73 +1,112 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView
 
-class MainWindow(QMainWindow):
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("IEC-870-5 Unbalanced Mode")
-        self.setGeometry(100, 100, 1000, 600)
+        self.setGeometry(100, 100, 800, 600)
 
-        central_widget = QWidget(self)
+        central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        layout = QVBoxLayout(central_widget)
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
 
-        self.create_section("SOSTAT", layout)
-        self.create_section("Conversor BitByte <-> PTNO", layout)
-        self.create_section("Localização/código de cores dos cabos", layout)
+        # Seção de Conversor BitByte <-> PTNO
+        sostat_label = QLabel("SOSTAT")
+        layout.addWidget(sostat_label)
 
-        self.create_table(layout)
+        conversor_label = QLabel("Conversor BitByte <-> PTNO")
+        layout.addWidget(conversor_label)
 
-    def create_section(self, title, layout):
-        label = QLabel(title)
-        layout.addWidget(label)
+        self.entry_bitbyte = QLineEdit()
+        layout.addWidget(self.entry_bitbyte)
 
-        if title == "Conversor BitByte <-> PTNO":
-            self.entry_bitbyte = self.create_input(layout)
-            self.create_button("Calcular PTNO", self.calculate_ptno, layout)
-            self.entry_ptno = self.create_input(layout)
-            self.create_button("Calcular Bit...", self.calculate_bitbyte, layout)
-        elif title == "Localização/código de cores dos cabos":
-            self.create_button("Localização das UTRs", self.display_tablertu, layout)
-            self.create_button("Código e Cores dos Cabos de UTRs", self.display_codigos_cores, layout)
+        calcular_ptno_button = QPushButton("Calcular PTNO")
+        layout.addWidget(calcular_ptno_button)
 
-    def create_input(self, layout):
-        entry = QLineEdit()
-        layout.addWidget(entry)
-        return entry
+        self.entry_ptno = QLineEdit()
+        layout.addWidget(self.entry_ptno)
 
-    def create_button(self, text, callback, layout):
-        btn = QPushButton(text)
-        layout.addWidget(btn)
-        btn.clicked.connect(callback)
+        calcular_bit_button = QPushButton("Calcular Bit...")
+        layout.addWidget(calcular_bit_button)
 
-    def create_table(self, layout):
+        limpar_button = QPushButton("Limpar valores")
+        layout.addWidget(limpar_button)
+
+        # Seção de Localização/código de cores dos cabos
+        localizacao_label = QLabel("Localização/código de cores dos cabos")
+        layout.addWidget(localizacao_label)
+
+        localizacao_button = QPushButton("Localização das UTRs")
+        layout.addWidget(localizacao_button)
+
+        cod_cores_button = QPushButton("Código e Cores dos Cabos de UTRs")
+        layout.addWidget(cod_cores_button)
+
+        # Tabela completa
+        self.table_frame = QWidget()
+        layout.addWidget(self.table_frame)
+
         self.table = QTableWidget()
-        layout.addWidget(self.table)
         self.table.setColumnCount(17)
-        self.table.setHorizontalHeaderLabels(["UTR", "Code", "Logic", "Link", "Local", "Unidade", "Cota", "Eixo"])
-        header = self.table.horizontalHeader()
-        header.setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.table.setHorizontalHeaderLabels(["UTR", "Code", "Logic", "Link", "Local", "Unidade", "Cota", "Eixo"] * 2)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        layout_table = QVBoxLayout()
+        layout_table.addWidget(self.table)
+        self.table_frame.setLayout(layout_table)
 
-    def calculate_ptno(self):
-        # Implemente a lógica de cálculo para PTNO aqui
-        pass
+        # Botão Procurar Geral
+        self.create_button("Procurar Geral", self.procurar_geral, layout_table)
 
-    def calculate_bitbyte(self):
-        # Implemente a lógica de cálculo para BitByte aqui
-        pass
+        # Dados da tabela
+        data = [
+            ["UTR501", "A01R01", "1", "1", "CF", "U1", "108", "C-D", "", "UTR520", "C45A01", "20", "19", "GIS", "U2", "124", "A-B"],
+            ["UTR502", "A02R01", "2", "2", "CF", "U2", "108", "C-D", "", "UTR520-1", "C45A02", "21", "20", "GIS", "U3", "124", "A-B"],
+            ["UTR503", "A03R01", "3", "3", "CF", "U3", "108", "C-D", "", "UTR521", "C45A03", "22", "21", "GIS", "U5", "124", "A-B"],
+            ["UTR504", "A04R01", "4", "4", "CF", "U4", "108", "C-D", "", "UTR522", "C45A04", "23", "22", "GIS", "U8", "124", "A-B"],
+            ["UTR505", "A05R01", "5", "5", "CF", "U5", "108", "C-D", "", "UTR620", "D45A01", "24", "23", "GIS", "U12", "124", "A-B"],
+            ["UTR506", "A06R01", "6", "1", "CF", "U6", "108", "C-D", "", "UTR621", "D45A02", "25", "24", "GIS", "U14", "124", "A-B"],
+            ["UTR507", "A07R01", "7", "7", "CF", "U7", "108", "C-D", "", "UTR622", "D45A03", "26", "25", "GIS", "U16", "124", "A-B"],
+            ["UTR508", "A08R01", "8", "8", "CF", "U8", "108", "C-D", "", "UTR622-1", "D45A04", "27", "26", "GIS", "U17", "124", "A-B"],
+            ["UTR509", "A09R01", "9", "9", "CF", "U9", "108", "C-D", "", "UTR530", "J61A01", "30", "27", "CF", "U3", "115", "C-D"],
+            ["UTR610", "B10R01", "10", "10", "CF", "U10", "108", "C-D", "", "UTR531", "J61A02", "31", "28", "CF", "U8", "115", "C-D"],
+            ["UTR611", "B11R01", "11", "11", "CF", "U11", "108", "C-D", "", "UTR532", "J61A03", "32", "29", "CF", "AMD2", "127,6", "C-D"],
+            ["UTR612", "B12R01", "12", "12", "CF", "U12", "108", "C-D", "", "UTR533", "J61A04", "33", "30", "CF", "AMD3", "144,5", "A-B"],
+            ["UTR613", "B13R01", "13", "13", "CF", "U13", "108", "C-D", "", "UTR534", "J61A05", "34", "31", "CF", "U7", "127,6", "C-D"],
+            ["UTR614", "B14R01", "14", "14", "CF", "U14", "108", "C-D", "", "UTR534-1", "J61A06", "35", "32", "GIS", "U9A", "132", "A-B"],
+            ["UTR615", "B15R01", "15", "15", "CF", "U15", "108", "C-D", "", "UTR630", "K61A01", "36", "33", "CF", "U12", "115", "C-D"],
+            ["UTR616", "B16R01", "16", "16", "CF", "U16", "108", "C-D", "", "UTR631", "K61A02", "37", "34", "CF", "AMC2", "127,6", "C-D"],
+            ["UTR617", "B17R01", "17", "17", "CF", "U17", "108", "C-D", "", "UTR632", "K61A03", "38", "35", "CF", "U16", "115", "C-D"],
+            ["UTR618", "B18R01", "18", "18", "CF", "U18", "108", "C-D", "", "UTR633", "K61A04", "39", "36", "CF", "AMC1", "144,5", "A-B"],
+            ["UTR501-1", "A01R02", "51", "43", "CF", "U1", "98", "A-B", "", "UTR634", "K61A05", "40", "37", "CF", "U15", "115", "C-D"],
+            ["UTR502-1", "A02R02", "52", "44", "CF", "U2", "98", "A-B", "", "UTR540", "J61A07", "41", "38", "Bar.", "U3", "214", ""],
+            ["UTR503-1", "A03R02", "53", "45", "CF", "U3", "98", "A-B", "", "UTR541", "J61A08", "42", "39", "Bar.", "U9", "144,5", ""],
+            ["UTR504-1", "A04R02", "54", "46", "CF", "U4", "98", "A-B", "", "UTR640", "K61A06", "43", "40", "Bar.", "U13", "214", ""],
+            ["UTR505-1", "A05R02", "55", "47", "CF", "U5", "98", "A-B", "", "UTR641", "K61A07", "44", "41", "Bar.", "U16", "214", ""],
+            ["UTR506-1", "A06R02", "56", "48", "CF", "U6", "98", "A-B", "", "UTR550", "H20A01", "80", "62", "Vert.", "", "214", ""],
+            ["UTR507-1", "A07R02", "57", "49", "CF", "U7", "98", "A-B", "", "UTR570-2", "T75A04", "81", "63", "CF", "U10", "131", "C-D"],
+            ["UTR508-1", "A08R02", "58", "50", "CF", "U8", "98", "A-B", "", "UTR670-1", "T75A05", "82", "64", "CF", "U9A", "131", "C-D"],
+            ["UTR509-1", "A09R02", "59", "51", "CF", "U9", "98", "A-B", "", "UTR570-1", "T75A03", "84", "70", "CF", "U9A", "131", "C-D"],
+            ["UTR610-1", "B10R02", "60", "52", "CF", "U10", "98", "A-B", "", "UTR670-2", "T75A06", "87", "71", "CF", "U10", "131", "C-D"],
+            ["UTR611-1", "B11R02", "61", "53", "CF", "U11", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR612-1", "B12R02", "62", "54", "CF", "U12", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR613-1", "B13R02", "63", "55", "CF", "U13", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR614-1", "B14R02", "64", "56", "CF", "U14", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR615-1", "B15R02", "65", "57", "CF", "U15", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR616-1", "B16R02", "66", "58", "CF", "U16", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR617-1", "B17R02", "67", "59", "CF", "U17", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR618-1", "B18R02", "68", "60", "CF", "U18", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+        ]
 
-    def display_tablertu(self):
-        # Esta função será chamada quando o botão "Localização das UTRs" for pressionado
-        pass
+        # Preencher a tabela com os dados
+        self.table.setRowCount(len(data))
+        for row, rowData in enumerate(data):
+            for col, value in enumerate(rowData):
+                item = QTableWidgetItem(value)
+                self.table.setItem(row, col, item)
 
-    def display_codigos_cores(self):
-        # Esta função exibirá a tabela com os códigos de cores
-        pass
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    def create_button(self, text, function, layout):
+        button = QPushButton(text)
+       
--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -1,118 +1,126 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView
 
-def exibir_tablertu():
-    # Esta função será chamada quando o botão "Localização das UTRs" for pressionado
-    pass
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("IEC-870-5 Unbalanced Mode")
+        self.setGeometry(100, 100, 800, 600)
 
-def calcular_ptno():
-    # Implemente a lógica de cálculo para PTNO aqui
-    pass
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-def calcular_bitbyte():
-    # Implemente a lógica de cálculo para BitByte aqui
-    pass
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
 
-def exibir_codigos_cores():
-    # Esta função exibirá a tabela com os códigos de cores
-    pass
+        # Seção de Conversor BitByte <-> PTNO
+        sostat_label = QLabel("SOSTAT")
+        layout.addWidget(sostat_label)
 
-app = tk.Tk()
-app.title("IEC-870-5 Unbalanced Mode")
+        conversor_label = QLabel("Conversor BitByte <-> PTNO")
+        layout.addWidget(conversor_label)
 
-frame_principal = ttk.Frame(app)
-frame_principal.pack(padx=10, pady=10)
+        self.entry_bitbyte = QLineEdit()
+        layout.addWidget(self.entry_bitbyte)
 
-# Seção de Conversor BitByte <-> PTNO
-label_sostat = ttk.Label(frame_principal, text="SOSTAT")
-label_sostat.grid(row=0, column=0, sticky="w", pady=(0, 10))
+        calcular_ptno_button = QPushButton("Calcular PTNO")
+        layout.addWidget(calcular_ptno_button)
 
-label_conversor = ttk.Label(frame_principal, text="Conversor BitByte <-> PTNO")
-label_conversor.grid(row=1, column=0, sticky="w", pady=(0, 5))
+        self.entry_ptno = QLineEdit()
+        layout.addWidget(self.entry_ptno)
 
-entry_bitbyte = ttk.Entry(frame_principal)
-entry_bitbyte.grid(row=2, column=0, sticky="w", pady=5)
+        calcular_bit_button = QPushButton("Calcular Bit...")
+        layout.addWidget(calcular_bit_button)
 
-btn_calc_ptno = ttk.Button(frame_principal, text="Calcular PTNO", command=calcular_ptno)
-btn_calc_ptno.grid(row=2, column=1, padx=5, pady=5)
+        limpar_button = QPushButton("Limpar valores")
+        layout.addWidget(limpar_button)
 
-entry_ptno = ttk.Entry(frame_principal)
-entry_ptno.grid(row=3, column=0, sticky="w", pady=5)
+        # Seção de Localização/código de cores dos cabos
+        localizacao_label = QLabel("Localização/código de cores dos cabos")
+        layout.addWidget(localizacao_label)
 
-btn_calc_bit = ttk.Button(frame_principal, text="Calcular Bit...", command=calcular_bitbyte)
-btn_calc_bit.grid(row=3, column=1, padx=5, pady=5)
+        localizacao_button = QPushButton("Localização das UTRs")
+        layout.addWidget(localizacao_button)
 
-btn_limpar = ttk.Button(frame_principal, text="Limpar valores")
-btn_limpar.grid(row=4, column=0, columnspan=2, pady=(5, 10))
+        cod_cores_button = QPushButton("Código e Cores dos Cabos de UTRs")
+        layout.addWidget(cod_cores_button)
 
-# Seção de Localização/código de cores dos cabos
-label_localizacao = ttk.Label(frame_principal, text="Localização/código de cores dos cabos")
-label_localizacao.grid(row=5, column=0, sticky="w", pady=(0, 5))
+        # Tabela completa
+        self.table_frame = QWidget()
+        layout.addWidget(self.table_frame)
 
-btn_utrs = ttk.Button(frame_principal, text="Localização das UTRs", command=exibir_tablertu)
-btn_utrs.grid(row=6, column=0, sticky="w", pady=5)
+        self.table = QTableWidget()
+        self.table.setColumnCount(17)
+        self.table.setHorizontalHeaderLabels(["UTR", "Code", "Logic", "Link", "Local", "Unidade", "Cota", "Eixo"] * 2)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        layout_table = QVBoxLayout()
+        layout_table.addWidget(self.table)
+        self.table_frame.setLayout(layout_table)
 
-btn_cod_cores = ttk.Button(frame_principal, text="Código e Cores dos Cabos de UTRs", command=exibir_codigos_cores)
-btn_cod_cores.grid(row=7, column=0, sticky="w", pady=5)
+        # Botão Procurar Geral
+        self.create_button("Procurar Geral", self.procurar_geral, layout_table)
 
-# Tabela completa
-table_data = [
-    ("UTR501", "A01R01", "1", "1", "CF", "U1", "108", "C-D", "", "UTR520", "C45A01", "20", "19", "GIS", "U2", "124", "A-B"),
-    ("UTR502", "A02R01", "2", "2", "CF", "U2", "108", "C-D", "", "UTR520-1", "C45A02", "21", "20", "GIS", "U3", "124", "A-B"),
-    ("UTR503", "A03R01", "3", "3", "CF", "U3", "108", "C-D", "", "UTR521", "C45A03", "22", "21", "GIS", "U5", "124", "A-B"),
-    ("UTR504", "A04R01", "4", "4", "CF", "U4", "108", "C-D", "", "UTR522", "C45A04", "23", "22", "GIS", "U8", "124", "A-B"),
-    ("UTR505", "A05R01", "5", "5", "CF", "U5", "108", "C-D", "", "UTR620", "D45A01", "24", "23", "GIS", "U12", "124", "A-B"),
-    ("UTR506", "A06R01", "6", "1", "CF", "U6", "108", "C-D", "", "UTR621", "D45A02", "25", "24", "GIS", "U14", "124", "A-B"),
-    ("UTR507", "A07R01", "7", "7", "CF", "U7", "108", "C-D", "", "UTR622", "D45A03", "26", "25", "GIS", "U16", "124", "A-B"),
-    ("UTR508", "A08R01", "8", "8", "CF", "U8", "108", "C-D", "", "UTR622-1", "D45A04", "27", "26", "GIS", "U17", "124", "A-B"),
-    ("UTR509", "A09R01", "9", "9", "CF", "U9", "108", "C-D", "", "UTR530", "J61A01", "30", "27", "CF", "U3", "115", "C-D"),
-    ("UTR610", "B10R01", "10", "10", "CF", "U10", "108", "C-D", "", "UTR531", "J61A02", "31", "28", "CF", "U8", "115", "C-D"),
-    ("UTR611", "B11R01", "11", "11", "CF", "U11", "108", "C-D", "", "UTR532", "J61A03", "32", "29", "CF", "AMD2", "127,6", "C-D"),
-    ("UTR612", "B12R01", "12", "12", "CF", "U12", "108", "C-D", "", "UTR533", "J61A04", "33", "30", "CF", "AMD3", "144,5", "A-B"),
-    ("UTR613", "B13R01", "13", "13", "CF", "U13", "108", "C-D", "", "UTR534", "J61A05", "34", "31", "CF", "U7", "127,6", "C-D"),
-    ("UTR614", "B14R01", "14", "14", "CF", "U14", "108", "C-D", "", "UTR534-1", "J61A06", "35", "32", "GIS", "U9A", "132", "A-B"),
-    ("UTR615", "B15R01", "15", "15", "CF", "U15", "108", "C-D", "", "UTR630", "K61A01", "36", "33", "CF", "U12", "115", "C-D"),
-    ("UTR616", "B16R01", "16", "16", "CF", "U16", "108", "C-D", "", "UTR631", "K61A02", "37", "34", "CF", "AMC2", "127,6", "C-D"),
-    ("UTR617", "B17R01", "17", "17", "CF", "U17", "108", "C-D", "", "UTR632", "K61A03", "38", "35", "CF", "U16", "115", "C-D"),
-    ("UTR618", "B18R01", "18", "18", "CF", "U18", "108", "C-D", "", "UTR633", "K61A04", "39", "36", "CF", "AMC1", "144,5", "A-B"),
-    ("UTR501-1", "A01R02", "51", "43", "CF", "U1", "98", "A-B", "", "UTR634", "K61A05", "40", "37", "CF", "U15", "115", "C-D"),
-    ("UTR502-1", "A02R02", "52", "44", "CF", "U2", "98", "A-B", "", "UTR540", "J61A07", "41", "38", "Bar.", "U3", "214", ""),
-    ("UTR503-1", "A03R02", "53", "45", "CF", "U3", "98", "A-B", "", "UTR541", "J61A08", "42", "39", "Bar.", "U9", "144,5", ""),
-    ("UTR504-1", "A04R02", "54", "46", "CF", "U4", "98", "A-B", "", "UTR640", "K61A06", "43", "40", "Bar.", "U13", "214", ""),
-    ("UTR505-1", "A05R02", "55", "47", "CF", "U5", "98", "A-B", "", "UTR641", "K61A07", "44", "41", "Bar.", "U16", "214", ""),
-    ("UTR506-1", "A06R02", "56", "48", "CF", "U6", "98", "A-B", "", "UTR550", "H20A01", "80", "62", "Vert.", "", "214", ""),
-    ("UTR507-1", "A07R02", "57", "49", "CF", "U7", "98", "A-B", "", "UTR570-2", "T75A04", "81", "63", "CF", "U10", "131", "C-D"),
-    ("UTR508-1", "A08R02", "58", "50", "CF", "U8", "98", "A-B", "", "UTR670-1", "T75A05", "82", "64", "CF", "U9A", "131", "C-D"),
-    ("UTR509-1", "A09R02", "59", "51", "CF", "U9", "98", "A-B", "", "UTR570-1", "T75A03", "84", "70", "CF", "U9A", "131", "C-D"),
-    ("UTR610-1", "B10R02", "60", "52", "CF", "U10", "98", "A-B", "", "UTR670-2", "T75A06", "87", "71", "CF", "U10", "131", "C-D"),
-    ("UTR611-1", "B11R02", "61", "53", "CF", "U11", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR612-1", "B12R02", "62", "54", "CF", "U12", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR613-1", "B13R02", "63", "55", "CF", "U13", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR614-1", "B14R02", "64", "56", "CF", "U14", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR615-1", "B15R02", "65", "57", "CF", "U15", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR616-1", "B16R02", "66", "58", "CF", "U16", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR617-1", "B17R02", "67", "59", "CF", "U17", "98", "A-B", "", "", "", "", "", "", "", "", ""),
-    ("UTR618-1", "B18R02", "68", "60", "CF", "U18", "98", "A-B", "", "", "", "", "", "", "", "", "")
-]
+        # Dados da tabela
+        data = [
+            ["UTR501", "A01R01", "1", "1", "CF", "U1", "108", "C-D", "", "UTR520", "C45A01", "20", "19", "GIS", "U2", "124", "A-B"],
+            ["UTR502", "A02R01", "2", "2", "CF", "U2", "108", "C-D", "", "UTR520-1", "C45A02", "21", "20", "GIS", "U3", "124", "A-B"],
+            ["UTR503", "A03R01", "3", "3", "CF", "U3", "108", "C-D", "", "UTR521", "C45A03", "22", "21", "GIS", "U5", "124", "A-B"],
+            ["UTR504", "A04R01", "4", "4", "CF", "U4", "108", "C-D", "", "UTR522", "C45A04", "23", "22", "GIS", "U8", "124", "A-B"],
+            ["UTR505", "A05R01", "5", "5", "CF", "U5", "108", "C-D", "", "UTR620", "D45A01", "24", "23", "GIS", "U12", "124", "A-B"],
+            ["UTR506", "A06R01", "6", "1", "CF", "U6", "108", "C-D", "", "UTR621", "D45A02", "25", "24", "GIS", "U14", "124", "A-B"],
+            ["UTR507", "A07R01", "7", "7", "CF", "U7", "108", "C-D", "", "UTR622", "D45A03", "26", "25", "GIS", "U16", "124", "A-B"],
+            ["UTR508", "A08R01", "8", "8", "CF", "U8", "108", "C-D", "", "UTR622-1", "D45A04", "27", "26", "GIS", "U17", "124", "A-B"],
+            ["UTR509", "A09R01", "9", "9", "CF", "U9", "108", "C-D", "", "UTR530", "J61A01", "30", "27", "CF", "U3", "115", "C-D"],
+            ["UTR610", "B10R01", "10", "10", "CF", "U10", "108", "C-D", "", "UTR531", "J61A02", "31", "28", "CF", "U8", "115", "C-D"],
+            ["UTR611", "B11R01", "11", "11", "CF", "U11", "108", "C-D", "", "UTR532", "J61A03", "32", "29", "CF", "AMD2", "127,6", "C-D"],
+            ["UTR612", "B12R01", "12", "12", "CF", "U12", "108", "C-D", "", "UTR533", "J61A04", "33", "30", "CF", "AMD3", "144,5", "A-B"],
+            ["UTR613", "B13R01", "13", "13", "CF", "U13", "108", "C-D", "", "UTR534", "J61A05", "34", "31", "CF", "U7", "127,6", "C-D"],
+            ["UTR614", "B14R01", "14", "14", "CF", "U14", "108", "C-D", "", "UTR534-1", "J61A06", "35", "32", "GIS", "U9A", "132", "A-B"],
+            ["UTR615", "B15R01", "15", "15", "CF", "U15", "108", "C-D", "", "UTR630", "K61A01", "36", "33", "CF", "U12", "115", "C-D"],
+            ["UTR616", "B16R01", "16", "16", "CF", "U16", "108", "C-D", "", "UTR631", "K61A02", "37", "34", "CF", "AMC2", "127,6", "C-D"],
+            ["UTR617", "B17R01", "17", "17", "CF", "U17", "108", "C-D", "", "UTR632", "K61A03", "38", "35", "CF", "U16", "115", "C-D"],
+            ["UTR618", "B18R01", "18", "18", "CF", "U18", "108", "C-D", "", "UTR633", "K61A04", "39", "36", "CF", "AMC1", "144,5", "A-B"],
+            ["UTR501-1", "A01R02", "51", "43", "CF", "U1", "98", "A-B", "", "UTR634", "K61A05", "40", "37", "CF", "U15", "115", "C-D"],
+            ["UTR502-1", "A02R02", "52", "44", "CF", "U2", "98", "A-B", "", "UTR540", "J61A07", "41", "38", "Bar.", "U3", "214", ""],
+            ["UTR503-1", "A03R02", "53", "45", "CF", "U3", "98", "A-B", "", "UTR541", "J61A08", "42", "39", "Bar.", "U9", "144,5", ""],
+            ["UTR504-1", "A04R02", "54", "46", "CF", "U4", "98", "A-B", "", "UTR640", "K61A06", "43", "40", "Bar.", "U13", "214", ""],
+            ["UTR505-1", "A05R02", "55", "47", "CF", "U5", "98", "A-B", "", "UTR641", "K61A07", "44", "41", "Bar.", "U16", "214", ""],
+            ["UTR506-1", "A06R02", "56", "48", "CF", "U6", "98", "A-B", "", "UTR550", "H20A01", "80", "62", "Vert.", "", "214", ""],
+            ["UTR507-1", "A07R02", "57", "49", "CF", "U7", "98", "A-B", "", "UTR570-2", "T75A04", "81", "63", "CF", "U10", "131", "C-D"],
+            ["UTR508-1", "A08R02", "58", "50", "CF", "U8", "98", "A-B", "", "UTR670-1", "T75A05", "82", "64", "CF", "U9A", "131", "C-D"],
+            ["UTR509-1", "A09R02", "59", "51", "CF", "U9", "98", "A-B", "", "UTR570-1", "T75A03", "84", "70", "CF", "U9A", "131", "C-D"],
+            ["UTR610-1", "B10R02", "60", "52", "CF", "U10", "98", "A-B", "", "UTR670-2", "T75A06", "87", "71", "CF", "U10", "131", "C-D"],
+            ["UTR611-1", "B11R02", "61", "53", "CF", "U11", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR612-1", "B12R02", "62", "54", "CF", "U12", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR613-1", "B13R02", "63", "55", "CF", "U13", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR614-1", "B14R02", "64", "56", "CF", "U14", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR615-1", "B15R02", "65", "57", "CF", "U15", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR616-1", "B16R02", "66", "58", "CF", "U16", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR617-1", "B17R02", "67", "59", "CF", "U17", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+            ["UTR618-1", "B18R02", "68", "60", "CF", "U18", "98", "A-B", "", "", "", "", "", "", "", "", ""],
+        ]
 
+        # Preencher a tabela com os dados
+        self.table.setRowCount(len(data))
+        for row, rowData in enumerate(data):
+            for col, value in enumerate(rowData):
+                item = QTableWidgetItem(value)
+                self.table.setItem(row, col, item)
 
-table_frame = ttk.Frame(frame_principal)
-table_frame.grid(row=8, column=0, columnspan=2, pady=10)
+    def create_button(self, text, function, layout):
+        button = QPushButton(text)
+        button.clicked.connect(function)
+        layout.addWidget(button)
 
-table = ttk.Treeview(table_frame, columns=[""] * 17, show="headings")
-table.heading("#1", text="UTR")
-table.heading("#2", text="Code")
-table.heading("#3", text="Logic")
-table.heading("#4", text="Link")
-table.heading("#5", text="Local")
-table.heading("#6", text="Unidade")
-table.heading("#7", text="Cota")
-table.heading("#8", text="Eixo")
-# Adicione mais cabeçalhos aqui
+    def procurar_geral(self):
+        # Lógica para o botão "Procurar Geral" aqui
+        pass
 
-for row in table_data:
-    table.insert("", "end", values=row)
+def main():
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec())
 
-table.pack()
-
-app.mainloop()
+if __name__ == "__main__":
+    main()
